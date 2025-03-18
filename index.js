2e92ef075c12f13020869ea5a796ec11eb3f6a1d
GunawanMayo <Qwanser3301@gmail.com>
2025-03-18 21:18:34 +0900

    To do listing web

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("hello");
+const todoInputText = document.getElementById('input_todo');
+const todoAddButton = document.getElementById('add_todo');
+const todoList = document.getElementById("todo_list");
+
+function checkIfEmpty() {
+  const emptyMessage = document.getElementById("empty_message");
+  if (todoList.children.length === 0) {
+    emptyMessage.style.display = "block";
+  } else {
+    emptyMessage.style.display = "none";
+  }
+}
+
+todoAddButton.addEventListener('click', () => {
+  const inputValue = todoInputText.value.trim();
+
+  if (inputValue === "") return;
+
+  const listItem = document.createElement("li");
+  listItem.className = "todo-li";
+  listItem.innerHTML = inputValue;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.type = "button";
+
+  listItem.appendChild(deleteButton);
+  todoList.appendChild(listItem);
+
+  todoInputText.value = "";
+
+  checkIfEmpty();
+
+  deleteButton.addEventListener('click', () => {
+    console.log("clicking delete button");
+    listItem.remove();
+    checkIfEmpty();
+  });
+});
+
+// checkIfEmpty();
